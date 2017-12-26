5510ea4d771fa4f234fb7f63add0d6b6bb574c14
wanglianghai <1540446221@qq.com>
2017-12-26 16:55:30 +0800

    渲染子目录

--- routes/particular/signup.js
+++ routes/particular/signup.js
@@ -2,7 +2,7 @@ const express = require('express')
 const router = express.Router()
 
 router.get('/', (req, res) => {
-  res.render('signup')
+  res.render('./particular/signup')
 })
 
 module.exports = router
