e0887a8160e5d96cfb6cf6dce68900ea70e18cdf
wanglianghai <1540446221@qq.com>
2017-12-27 10:14:38 +0800

    修改端口显示

--- index.js
+++ index.js
@@ -2,9 +2,20 @@ const express = require('express')
 const app = express() //模块中的一个函数
 const indexRouter = require('./routes/index')
 const path = require('path')
+const config = require('config-lite')(__dirname)
+const pkg = require('./package')
 
+
+// 设置视图目录
 app.set('views', path.join(__dirname, 'views'))
+// 设置视图引擎为 ejs
 app.set('view engine', 'pug')
 
+// 设置服务器静态文件目录
+app.use(express.static(path.join(__dirname, 'public')))
+
 indexRouter(app)
-app.listen(3000)
+
+app.listen(config.port, () => {
+  console.log(`${pkg.name} listen on ${config.port}`)
+})
