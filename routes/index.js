8b8275ffac8efe854764863e2a65771b7bfe1018
wanglianghai <1540446221@qq.com>
2017-12-29 09:48:19 +0800

    页面跳转

--- routes/index.js
+++ routes/index.js
@@ -1,3 +1,7 @@
 module.exports = (app) => {
+  app.get('/', (req, res) => {
+    res.redirect('/article')
+  })
+  app.use('/article', require('./particular/article'))
   app.use('/signup', require('./particular/signup'))
 }
